315c75d5d3da85d27d050e507872d2b7f4278749
michaelrhodes <spam@michaelrhod.es>
2019-01-13 14:25:54 +1000

    Make test file more readable

--- .test.ci.js
+++ .test.ci.js
@@ -3,7 +3,7 @@ var exitCode = 0
 
 spy.on('assert', function (ok, message) {
   if (!ok) exitCode = 1
-  if (ok && message === 'EOT') process.exit(exitCode)
+  if (ok === 'EOT') process.exit(exitCode)
 })
 
 require('./test')
--- test.js
+++ test.js
@@ -1,32 +1,34 @@
+var assert = console.assert.bind(console)
 var otv = require('./')
+
 var value, val
 
 // Basic usage
 value = otv(generator)
-console.assert(value() === null, 'Undefined key returns null')
-console.assert(value(null) === null, 'Null returns null')
-console.assert(val = value('key'), 'Non-null key returns value')
-console.assert(val === value('key'), 'Value persists')
-console.assert(value.verify('key', val), 'Value can be verified')
-console.assert(val !== value('key'), 'Value changes after verification')
+assert(value() === null, 'Undefined key returns null')
+assert(value(null) === null, 'Null returns null')
+assert(val = value('key'), 'Non-null key returns value')
+assert(val === value('key'), 'Value persists')
+assert(value.verify('key', val), 'Value can be verified')
+assert(val !== value('key'), 'Value changes after verification')
 val = value('key')
-console.assert(!value.verify('key', value('_')), 'Incorrect value fails verification')
-console.assert(!value.verify('key'), 'Undefined value fails verification')
-console.assert(val === value('key'), 'Value doesn’t change after verification failure')
+assert(!value.verify('key', value('_')), 'Incorrect value fails verification')
+assert(!value.verify('key'), 'Undefined value fails verification')
+assert(val === value('key'), 'Value doesn’t change after verification failure')
 
 // With TTL
 value = otv(generator, 100)
 val = value('key')
 
-console.assert(val === value('key'), 'Value still persists')
+assert(val === value('key'), 'Value still persists')
 
 setTimeout(function () {
-  console.assert(value.verify('key', val), 'Value can be verified before expiry')
+  assert(value.verify('key', val), 'Value can be verified before expiry')
 
   val = value('key')
   setTimeout(function () {
-    console.assert(!value.verify('key', val), 'Value can’t be verified after expiry')
-    console.assert(true, 'EOT')
+    assert(!value.verify('key', val), 'Value can’t be verified after expiry')
+    assert('EOT')
   }, 150)
 }, 50)
 
