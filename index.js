5067610ce5870e2b5be5f7df67a22f330108f4c9
michaelrhodes <spam@michaelrhod.es>
2020-08-23 19:03:43 +1000

    Use Map instead of Object

--- index.js
+++ index.js
@@ -1,44 +1,44 @@
 module.exports = otv
 
 function otv (generator, ttl) {
-  var store = {}
+  var store = new Map
   value.verify = verify
   value.expiry = expiry
   return value
 
   function value (key) {
     return key != null ? (active(key) ?
-      store[key].value :
+      store.get(key).value :
       generate(key).value) :
       null
   }
 
   function verify (key, value) {
     return Boolean(active(key) &&
-      store[key].value === value &&
-      delete store[key])
+      store.get(key).value === value &&
+      store.delete(key))
   }
 
   function expiry (key) {
     return ttl &&
       active(key) &&
-      (store[key].time + ttl) ||
+      (store.get(key).time + ttl) ||
       null
   }
 
   function active (key) {
     return key != null && Boolean(ttl ? (
-      store[key] &&
-      store[key].time != null &&
-      store[key].value != null &&
-      (Date.now() - store[key].time <= ttl)) :
-      (store[key] && store[key].value != null))
+      store.has(key) &&
+      store.get(key).time != null &&
+      store.get(key).value != null &&
+      (Date.now() - store.get(key).time <= ttl)) :
+      (store.has(key) && store.get(key).value != null))
   }
 
   function generate (key) {
-    return store[key] = {
+    return store.set(key, {
       time: Date.now(),
       value: generator()
-    }
+    }).get(key)
   }
 }
